9fe273c210382487b2d6c8fd8046ba24f2991e23
Fernando Matamoros <fmatamoros202@gmail.com>
2021-10-16 00:07:16 -0400

    Added the responsive design styles on the shows page.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -91,45 +91,63 @@ function createShows (showsArray) {
     modularContainer.classList.add('modular-container');
     containerShows.appendChild(modularContainer);
 
+    let modularSubContainer1 = document.createElement('div');
+    modularSubContainer1.classList.add('modular-subcontainer1');
+
     let dateTag = document.createElement('p');
-    dateTag.classList.add("modular-container__dateTag");
+    dateTag.classList.add("modular-subcontainer1__dateTag");
     dateTag.innerHTML = showsArray.dateTag;
-    modularContainer.appendChild(dateTag);
 
     let dateInfo = document.createElement('p');
-    dateInfo.classList.add("modular-container__dateInfo");
+    dateInfo.classList.add("modular-subcontainer1__dateInfo");
     dateInfo.innerHTML = showsArray.dateInfo;
-    modularContainer.appendChild(dateInfo);
+    modularSubContainer1.appendChild(dateTag);
+    modularSubContainer1.appendChild(dateInfo);
+
+    let modularSubContainer2 = document.createElement('div');
+    modularSubContainer2.classList.add('modular-subcontainer2');
 
     let venueTag = document.createElement('p');
-    venueTag.classList.add("modular-container__venueTag");
+    venueTag.classList.add("modular-subcontainer2__venueTag");
     venueTag.innerHTML = showsArray.venueTag;
-    modularContainer.appendChild(venueTag);
 
     let venue = document.createElement('p');
-    venue.classList.add("modular-container__venue");
+    venue.classList.add("modular-subcontainer2__venue");
     venue.innerHTML = showsArray.venue;
-    modularContainer.appendChild(venue);
+    modularSubContainer2.appendChild(venueTag);
+    modularSubContainer2.appendChild(venue);
+
+    let modularSubContainer3 = document.createElement('div');
+    modularSubContainer3.classList.add('modular-subcontainer3');
 
     let locationTag = document.createElement('p');
-    locationTag.classList.add("modular-container__locationTag");
+    locationTag.classList.add("modular-subcontainer3__locationTag");
     locationTag.innerHTML = showsArray.locationTag;
-    modularContainer.appendChild(locationTag);
+    
 
     let location = document.createElement('p');
-    location.classList.add("modular-container__location");
+    location.classList.add("modular-subcontainer3__location");
     location.innerHTML = showsArray.location;
-    modularContainer.appendChild(location);
+    modularSubContainer3.appendChild(locationTag);
+    modularSubContainer3.appendChild(location);
+
+    let modularSubContainer4 = document.createElement('div');
+    modularSubContainer4.classList.add('modular-subcontainer4');
 
     let ctaTag = document.createElement('p');
-    ctaTag.classList.add("modular-container__ctaTag");
+    ctaTag.classList.add("modular-subcontainer4__ctaTag");
     ctaTag.innerHTML = showsArray.ctaTag;
-    modularContainer.appendChild(ctaTag);
 
     let cta = document.createElement('a');
-    cta.classList.add("modular-container__cta");
+    cta.classList.add("modular-subcontainer4__cta");
     cta.innerHTML = showsArray.cta;
-    modularContainer.appendChild(cta);
+    modularSubContainer4.appendChild(ctaTag);
+    modularSubContainer4.appendChild(cta);
+
+    modularContainer.appendChild(modularSubContainer1);
+    modularContainer.appendChild(modularSubContainer2);
+    modularContainer.appendChild(modularSubContainer3);
+    modularContainer.appendChild(modularSubContainer4);
 }
 
 showsArray.forEach((element) =>{
